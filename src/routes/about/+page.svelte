82a2c38dc49c8bdd0bd9a85d3404721981cfd8cc
clement <58054176+clm-msch@users.noreply.github.com>
2023-10-24 16:40:35 +0200

    🚧Builde card component (default UI)

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,26 +1,30 @@
+<script>
+	import ArticleCard from '../AboutCard.svelte';
+</script>
 <svelte:head>
 	<title>About</title>
 	<meta name="description" content="About this app" />
 </svelte:head>
 
-<div class="text-column bg-black">
-	<h1>About this app</h1>
 
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
+<ArticleCard 
+  title="Lorem Ipseum 1" 
+  date="Wed Sep 20 2023" 
+  description="Lorem ipseum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  url="https://www.google.com"
+/>
 
-	<pre>npm create svelte@latest</pre>
+<ArticleCard 
+  title="Lorem Ipseum 2" 
+  date="Mon Sep 18 2023" 
+  description="Lorem ipseum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  url="https://www.google.com"
+/>
 
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
+<ArticleCard 
+  title="Lorem Ipseum 3" 
+  date="Thu Sep 07 2023" 
+  description="Lorem ipseum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  url="https://www.google.com"
+/>
 
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
-	</p>
-</div>
